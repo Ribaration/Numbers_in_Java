48a254539f929c97381a0bc66b8754183a96162f
unknown <ribalerato97@gmail.com>
2024-02-06 02:08:05 +0200

    second commit

---
+++ src/main/java/com/mycompany/printnumbers/App.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.printnumbers;
+
+/**
+ *
+ * @author Ribaration
+ */
+public class App {
+
+    public static void main(String[] args) {
+        //we don't put numbers inside double quotes
+        
+        System.out.println(358);
+        System.out.println(3);
+        System.out.println(50000);
+        
+        //We can also perform mathematical calculations inside the println() method
+        //1.Addition Operation
+        System.out.println(3 + 3);
+        
+        //2.Subtraction Operation
+        System.out.println(4 - 3);
+        
+        //3.Division Operation
+        System.out.println(12 / 3);
+        
+        //4.Multiplication Operation      
+        System.out.println(4 * 3);
+
+
+       /* Multiple comments are listed within
+        @@@@@@@@@@@@@@@@@@
+        ##################
+        */
+
+    }
+}
